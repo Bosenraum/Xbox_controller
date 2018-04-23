1514e0e2814b79751153d372d6a0d8fd7f7cb9b6
Austin Rosenbaum <austinjrosenbaum@gmail.com>
2018-04-22 22:05:02 -0600

    Added a few more button inputs and max detection for sticks/triggers

--- test.py
+++ test.py
@@ -22,6 +22,10 @@ def decode_button_input(button, state):
         output = "Left stick "
     elif(button == "BTN_THUMBR"):
         output = "Right stick "
+    elif(button == "BTN_START"):
+        output = "Start button "
+    elif(button == "BTN_SELECT"):
+        output = "Select button "
     else:
         output = button + " "
 
@@ -72,6 +76,37 @@ while True:
 
         if dpad_match != None:
             decode_dpad_input(event.code, event.state)
+
+        if event.code == "ABS_X":
+            if event.state == 2**15 - 1:
+                print("LEFT STICK MAX RIGHT")
+            elif event.state == -(2**15):
+                print("LEFT STICK MAX LEFT")
+
+        elif event.code == "ABS_Y":
+            if event.state == 2**15 - 1:
+                print("LEFT STICK MAX UP")
+            elif event.state == -(2**15):
+                print("LEFT STICK MAX DOWN")
+
+        if event.code == "ABS_RX":
+            if event.state == 2**15 - 1:
+                print("RIGHT STICK MAX RIGHT")
+            elif event.state == -(2**15):
+                print("RIGHT STICK MAX LEFT")
+
+        elif event.code == "ABS_RY":
+            if event.state == 2**15 - 1:
+                print("RIGHT STICK MAX UP")
+            elif event.state == -(2**15):
+                print("RIGHT STICK MAX DOWN")
+
+        if event.code == "ABS_Z":
+            if event.state == 2**8 - 1:
+                print("LEFT TRIGGER MAX")
+        elif event.code == "ABS_RZ":
+            if event.state == 2**8 - 1:
+                print("RIGHT TRIGGER MAX")
 
             # if event.state != 0:
             #     print(f"EVENT CODE {event.code}")
